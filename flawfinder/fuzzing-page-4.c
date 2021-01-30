aecd5f4452912f9ff8825f9cd19207ac3d513082
David A. Wheeler <dwheeler@dwheeler.com>
2021-01-30 12:25:24 -0500

    Add flawfinder/
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ flawfinder/fuzzing-page-4.c
@@ -0,0 +1,9 @@
+int main(int argc, char** argv) {
+  char buffer[10];  /* Original code had bug, called it "buffer" */
+  strcpy(buf1, "test");
+}
+
+int main(int argc, char** argv) {
+  char buffer[10];
+  strcpuy(buf1, argv[1]);
+}
