d9aefe957cd062aa3cd70eefdeb6bca4d71040c4
David A. Wheeler <dwheeler@dwheeler.com>
2021-09-02 20:53:28 -0400

    Add link to numbers database
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ numbers-database/index-old.php
@@ -0,0 +1,6 @@
+<?php
+/* Redirect browser */
+header("Location: https://docs.google.com/spreadsheet/ccc?key=0AqAaBLBsShVbdEE0QnB6dFpha1NSWVdlNERiSjdKR1E");
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
